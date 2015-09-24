db50d042e73d408a17ebbb3a92106e86e199a663
Pawel Jankowski <jjpikoov@gmail.com>
2015-09-24 21:38:26 +0200

    Module created, twohorizontallayout created.

--- explorerr.py
+++ explorerr.py
@@ -1,16 +1,12 @@
 from kivy.app import App
-from kivy.app import Widget
-
-
-class Foo(Widget):
-    pass
+from view.layouts.twohorizontallayout import TwoHorizontalLayout
 
 
 class ExplorerrApp(App):
     """Starting class"""
     def build(self):
         """This stuff will be run"""
-        return Foo()
+        return TwoHorizontalLayout()
 
 if __name__ == '__main__':
     ExplorerrApp().run()
---
+++ view/layouts/twohorizontallayout.py
@@ -0,0 +1,6 @@
+from kivy.uix.gridlayout import GridLayout
+
+
+class TwoHorizontalLayout(GridLayout):
+    """Grid for text console screen and item selection menu"""
+    pass
