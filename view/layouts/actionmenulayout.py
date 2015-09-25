b383518a876db71e8723b4eaee72b29634e81314
Pawel Jankowski <jjpikoov@gmail.com>
2015-09-25 14:49:59 +0200

    Changing labels added.

--- view/layouts/actionmenulayout.py
+++ view/layouts/actionmenulayout.py
@@ -5,4 +5,7 @@ from kivy.lang import Builder
 class ActionMenuLayout(GridLayout):
     """Layout of actions menu, where user can tap on item"""
     Builder.load_file('./view/layouts/actionmenulayout.kv')
+
+    def set_label(self, label):
+        self.item_label = label
     pass
--- view/layouts/twohorizontallayout.py
+++ view/layouts/twohorizontallayout.py
@@ -1,9 +1,23 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from view.layouts.actionmenulayout import ActionMenuLayout
+from kivy.uix.label import Label
+# from kivy.properties import ObjectProperty
+
+
+class ItemLabel(Label):
+    def set_text(self, text):
+        self.text = text
+    pass
 
 
 class TwoHorizontalLayout(GridLayout):
     """Grid for text console screen and item selection menu"""
     Builder.load_file('./view/layouts/twohorizontallayout.kv')
+
+    def __init__(self, **kwargs):
+        super(TwoHorizontalLayout, self).__init__(**kwargs)
+        label = self.ids.item_label
+        action_menu_layout = self.ids.action_menu_layout
+        action_menu_layout.set_label(label)
     pass
