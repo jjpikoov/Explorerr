4b7bfcc18f1397310b7a4c6d9ce5a83c2ac11cd3
Pawel Jankowski <jjpikoov@gmail.com>
2015-09-25 15:48:47 +0200

    Now smartphones should vibrate when buttons are pressed.:wq

--- view/layouts/actionmenulayout.py
+++ view/layouts/actionmenulayout.py
@@ -1,5 +1,6 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
+from plyer.facades.vibrator import Vibrator
 
 
 class ActionMenuLayout(GridLayout):
@@ -8,4 +9,11 @@ class ActionMenuLayout(GridLayout):
 
     def set_label(self, label):
         self.item_label = label
+
+    def vibrate(self, seconds):
+        vibrator = Vibrator()
+        try:
+            vibrator.vibrate(seconds)
+        except NotImplementedError:
+            print "I can't vibrate, are you on computer?"
     pass
